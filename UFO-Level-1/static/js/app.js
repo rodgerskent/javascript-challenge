12700bd659b4c218c4c57a6173e0597b8fd99dd4
Kent Rodgers <rodgerskent@outlook.com>
2020-12-17 15:16:38 -0700

    Thu Afternoon Update

--- UFO-Level-1/static/js/app.js
+++ UFO-Level-1/static/js/app.js
@@ -1,26 +1,36 @@
 // from data.js
-var tableData = data;
+var startData = data;
+// var filteredData = []
+
+// Console.log the initial data set from data.js
+console.log(startData);
 
 // Get a reference to the table body
 var tbody = d3.select("tbody");
 
-// Console.log the weather data from data.js
-console.log(tableData);
-
-// Insert the unfiltered data into a table
-data.forEach((tableData) => {
+// Insert the starting (unfiltered) data into a table
+data.forEach((startData) => {
     var row = tbody.append("tr");
-    Object.entries(tableData).forEach(([key, value]) => {
+    Object.entries(startData).forEach(([key, value]) => {
       var cell = row.append("td");
       cell.text(value);
     });
   });
 
+// // Insert the starting (unfiltered) data into a table
+// data.forEach((filteredData) => {
+//   var row = tbody.append("tr");
+//   Object.entries(filteredData).forEach(([key, value]) => {
+//     var cell = row.append("td");
+//     cell.text(value);
+//   });
+// });
+
 // Select the button
-var button = d3.select("#button");
+var button = d3.select("#filter-btn");
 
 // Select the form
-var form = d3.select("#form");
+var form = d3.select("#datetime");
 
 // Create event handlers for clicking the button or pressing the enter key
 button.on("click", runEnter);
@@ -29,30 +39,51 @@ form.on("submit",runEnter);
 // Create the function to run for both events
 function runEnter() {
 
-    // Prevent the page from refreshing
-    d3.event.preventDefault();
-  
+    // // Prevent the page from refreshing
+    // d3.event.preventDefault();
+     
     // Select the input element and get the raw HTML node
     var inputElement = d3.select("#datetime");
-  
+    console.log(inputElement);
+
     // Get the value property of the input element
     var inputValue = inputElement.property("value");
-  
-    // Print the value to the console
     console.log(inputValue);
-      
+    
+    // var tableData = data;
+    // console.log(tableData);
+
    // Filter the data based on user input
-    var filteredData = tableData.filter(item => item.datetime === inputValue);
-
-    data.forEach((filteredData) => {
-        var row = tbody.append("tr");
-        Object.entries(filteredData).forEach(([key, value]) => {
-          var cell = row.append("td");
-          cell.text(value);
-        });
-      });
-
+    var filteredData = startData.filter(item => item.datetime === inputValue);
     console.log(filteredData);
+
+    // var startData = filteredData
+
+    // data.forEach((startData !== filteredData) => {
+    //   var row = tbody.remove("tr");
+    //   Object.entries(startData !== filteredData).forEach(([key, value]) => {
+    //     var cell = row.remove("td");
+    //     cell.text(value);
+    //   });
+    // });
+
+    // data.forEach((filteredData) => {
+    //     var row = tbody.append("tr");
+    //     Object.entries(filteredData).forEach(([key, value]) => {
+    //       var cell = row.append("td");
+    //       cell.text(value);
+    //     });
+    //   });
+    
+      // data.forEach((filteredData) => {
+      //   var row = tbody.replaceWith("tr");
+      //   Object.entries(filteredData).forEach(([key, value]) => {
+      //     var cell = row.replaceWith("td");
+      //     cell.text(value);
+      //   });
+      // });
+
+
   }
 
   
