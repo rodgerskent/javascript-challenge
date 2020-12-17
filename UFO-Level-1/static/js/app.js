3dbc6b3a6b278ddad2405cc61c1a95e98e6fc617
Kent Rodgers <rodgerskent@outlook.com>
2020-12-17 14:01:01 -0700

    Thu PM Update

--- UFO-Level-1/static/js/app.js
+++ UFO-Level-1/static/js/app.js
@@ -1,4 +1,58 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+// Get a reference to the table body
+var tbody = d3.select("tbody");
+
+// Console.log the weather data from data.js
+console.log(tableData);
+
+// Insert the unfiltered data into a table
+data.forEach((tableData) => {
+    var row = tbody.append("tr");
+    Object.entries(tableData).forEach(([key, value]) => {
+      var cell = row.append("td");
+      cell.text(value);
+    });
+  });
+
+// Select the button
+var button = d3.select("#button");
+
+// Select the form
+var form = d3.select("#form");
+
+// Create event handlers for clicking the button or pressing the enter key
+button.on("click", runEnter);
+form.on("submit",runEnter);
+
+// Create the function to run for both events
+function runEnter() {
+
+    // Prevent the page from refreshing
+    d3.event.preventDefault();
+  
+    // Select the input element and get the raw HTML node
+    var inputElement = d3.select("#datetime");
+  
+    // Get the value property of the input element
+    var inputValue = inputElement.property("value");
+  
+    // Print the value to the console
+    console.log(inputValue);
+      
+   // Filter the data based on user input
+    var filteredData = tableData.filter(item => item.datetime === inputValue);
+
+    data.forEach((filteredData) => {
+        var row = tbody.append("tr");
+        Object.entries(filteredData).forEach(([key, value]) => {
+          var cell = row.append("td");
+          cell.text(value);
+        });
+      });
+
+    console.log(filteredData);
+  }
+
+  
