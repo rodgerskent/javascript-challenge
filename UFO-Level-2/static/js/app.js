7f8bd6cadddcfda19aa7b3bc64287409fba00f00
Kent Rodgers <rodgerskent@outlook.com>
2020-12-28 16:37:01 -0700

    Mon Fifth Update

--- UFO-Level-2/static/js/app.js
+++ UFO-Level-2/static/js/app.js
@@ -20,9 +20,7 @@ data.forEach((startData) => {
 var button = d3.select("#filter-btn");
 
 // Select the form
-// var form = d3.select("#datetime");
 var form = d3.select("form");
-//var form2 = d3.select('#city')
 
 // Create event handlers for clicking the button or pressing the enter key
 //button.on("click", runEnter);
@@ -31,7 +29,7 @@ form.on("submit",runEnter);
 // Create the function to run for both events
 function runEnter() {
     // Clear earlier rows of data
-    tbody.html("");
+    // tbody.html("");
 
     // Prevent the page from refreshing
     d3.event.preventDefault();
@@ -42,6 +40,8 @@ function runEnter() {
     var inputElement3 = d3.select("#state");
     var inputElement4 = d3.select("#country");
     var inputElement5 = d3.select("#shape");
+    
+    // Show the inputElement in the console log for bug detection
     console.log(inputElement);
     console.log(inputElement2);
     console.log(inputElement3);
@@ -49,20 +49,22 @@ function runEnter() {
     console.log(inputElement5);
 
     // Get the value property of the input element
-    var idate = inputElement.property("value");
-    var icity = inputElement2.property("value");
-    var istate = inputElement3.property("value");
     var icountry = inputElement4.property("value");
+    var istate = inputElement3.property("value");
+    var icity = inputElement2.property("value");
+    var idate = inputElement.property("value");
     var ishape = inputElement5.property("value");
-    console.log(idate);
-    console.log(icity);
-    console.log(istate);
+    
+    // Show the value in the console log for bug detection
     console.log(icountry);
+    console.log(istate);
+    console.log(icity);
+    console.log(idate);    
     console.log(ishape);
 
 
   // Mon 12-28 nested filter
-    if (icountry)
+    if (icountry) { 
     // tbody.html("");
       if (icountry && istate) {
         // tbody.html("");
@@ -72,6 +74,7 @@ function runEnter() {
             // tbody.html("");
             if (icountry && istate && icity && idate && ishape) {
               var filteredData = startData.filter(item => item.country === icountry && item.state === istate && item.city === icity && item.datetime === idate && item.shape === ishape)
+              console.log('country first with all')
               console.log(filteredData);
               tbody.html("");
               filteredData.forEach((userinput) => {
@@ -83,6 +86,7 @@ function runEnter() {
               });
             }
               else var filteredData = startData.filter(item => item.country === icountry && item.state === istate && item.city === icity && item.datetime === idate)
+              console.log('country first then, state, city and date')
               console.log(filteredData);
               tbody.html("");
               filteredData.forEach((userinput) => {
@@ -94,6 +98,7 @@ function runEnter() {
                });
           }
             else var filteredData = startData.filter(item => item.country === icountry && item.state === istate && item.city === icity)
+            console.log('country first then state and city')
             console.log(filteredData);
             tbody.html("");
             filteredData.forEach((userinput) => {
@@ -105,6 +110,7 @@ function runEnter() {
             });
         }
           else var filteredData = startData.filter(item => item.country === icountry && item.state === istate)
+          console.log('country first then date')
           console.log(filteredData);
           tbody.html("");
           filteredData.forEach((userinput) => {
@@ -114,7 +120,7 @@ function runEnter() {
               cell.text(value);
             });
           });
-      }
+      }  
         else var filteredData = startData.filter(item => item.country === icountry)
         console.log(filteredData);
         tbody.html("");
@@ -125,9 +131,79 @@ function runEnter() {
             cell.text(value);
           });
         });
+    }   
   
+  // Mon 12-28 second filter brace
+    if (istate)
+    // tbody.html("");
+      if (icity && istate) {
+        // tbody.html("");
+        if (idate && istate && icity) {
+          // tbody.html("");
+          if (ishape && istate && icity && idate) {
+            // tbody.html("");
+            if (icountry && istate && icity && idate && ishape) {
+              var filteredData = startData.filter(item => item.country === icountry && item.state === istate && item.city === icity && item.datetime === idate && item.shape === ishape)
+              console.log('state first with all')
+              console.log(filteredData);
+              tbody.html("");
+              filteredData.forEach((userinput) => {
+                var row = tbody.append("tr");
+                Object.entries(userinput).forEach(([key, value]) => {
+                  var cell = row.append("td");
+                  cell.text(value);
+                });
+              });
+            }
+              else var filteredData = startData.filter(item => item.shape === ishape && item.state === istate && item.city === icity && item.datetime === idate)
+              console.log('state first then city, date and shape')
+              console.log(filteredData);
+              tbody.html("");
+              filteredData.forEach((userinput) => {
+                var row = tbody.append("tr");
+                Object.entries(userinput).forEach(([key, value]) => {
+                  var cell = row.append("td");
+                  cell.text(value);
+                });
+              });
+          }
+            else var filteredData = startData.filter(item => item.datetime === idate && item.state === istate && item.city === icity)
+            console.log('state first, then city, then date')
+            console.log(filteredData);
+            tbody.html("");
+            filteredData.forEach((userinput) => {
+              var row = tbody.append("tr");
+              Object.entries(userinput).forEach(([key, value]) => {
+                var cell = row.append("td");
+                cell.text(value);
+              });
+            });
+        }
+          else var filteredData = startData.filter(item => item.city === icity && item.state === istate)
+          console.log('state first then city')
+          console.log(filteredData);
+          tbody.html("");
+          filteredData.forEach((userinput) => {
+            var row = tbody.append("tr");
+            Object.entries(userinput).forEach(([key, value]) => {
+              var cell = row.append("td");
+              cell.text(value);
+            });
+          });
+      }
     
-
+        else var filteredData = startData.filter(item => item.state === istate)
+        console.log('state first and only')
+        console.log(filteredData);
+        tbody.html("");
+        filteredData.forEach((userinput) => {
+          var row = tbody.append("tr");
+          Object.entries(userinput).forEach(([key, value]) => {
+            var cell = row.append("td");
+            cell.text(value);
+          });
+        });  
+// End of Mon second brace
 }
     
     
